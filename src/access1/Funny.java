9da3740d755ecaffc0e012f959d11d10c607be3e
dilipkumardemo <dilipkon001@gmail.com>
2022-09-24 16:18:45 +0530

    jj

--- src/access1/Funny.java
+++ src/access1/Funny.java
@@ -7,8 +7,7 @@ public	 int id=23;
 public		 void mm() {
 		System.out.println("kingmaker");
 		
-
-		System.out.println("money is utimate");
+        System.out.println("money is utimate");
 		System.out.println("moner");
 
 	}
